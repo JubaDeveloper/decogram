ca871ae8090374dc8ab8e455c98eead14f4083d6
JubaDeveloper <michaelres1332@gmail.com>
2025-07-05 18:59:33 -0300

    index: Changed exports

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,23 @@
-// Import all core related modules
-import * as types from "./core/types/types";
-import * as telegramMounting from "./core/telegram/mounting/mounting";
-import * as strategyPolling from "./core/strategy/polling";
-import * as strategyWebhook from "./core/strategy/webhook";
-import * as singleton from "./core/singleton/singleton";
-import * as bootstrap from "./core/bootstrap/bootstrap";
+// Export core types with alias if necessary
+export * from "./core/types/types";
+export * from "./core/types/types";
 
-// Group and export all core modules under 'core'
-export const core = {
-  ...types,
-  telegramMounting,
-  strategy: {
-    polling: strategyPolling,
-    webhook: strategyWebhook,
-  },
-  singleton,
-  bootstrap,
-};
+// Export strategy modules
+export * from "./core/strategy/polling";
+export * from "./core/strategy/webhook";
 
+// Export core components
+export * from "./core/bootstrap/bootstrap";
+export * from "./core/singleton/singleton";
+export * from "./core/telegram/mounting/mounting";
 
-// Import all io decorators
-import * as ioClass from "./core/decorators/io/class";
-import * as ioParameter from "./core/decorators/io/parameter";
-import * as ioMethod from "./core/decorators/io/method";
-import * as ioError from "./core/decorators/io/error";
+// Export decorators - I/O
+export * from "./core/decorators/io/class";
+export * from "./core/decorators/io/parameter";
+export * from "./core/decorators/io/method";
+export * from "./core/decorators/io/error";
 
-// Group and export all io decorators under 'decorators.io'
-export const decoratorsIo = {
-  class: ioClass,
-  parameter: ioParameter,
-  method: ioMethod,
-  error: ioError,
-};
-
-
-// Import all iot decorators
-import * as iotAutowired from "./core/decorators/iot/autowired";
-import * as iotComponent from "./core/decorators/iot/component";
-import * as iotService from "./core/decorators/iot/service";
-
-// Group and export all iot decorators under 'decorators.iot'
-export const decoratorsIot = {
-  autowired: iotAutowired,
-  component: iotComponent,
-  service: iotService,
-};
+// Export decorators - IoT (Dependency Injection)
+export * from "./core/decorators/iot/autowired";
+export * from "./core/decorators/iot/component";
+export * from "./core/decorators/iot/service";
